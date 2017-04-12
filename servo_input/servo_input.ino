8d65c3366f8bce6101c47aca57b8877837b90189
norio oya <nor.0721@gmail.com>
2017-04-12 22:51:50 +0900

    offset

--- servo_input/servo_input.ino
+++ servo_input/servo_input.ino
@@ -2,16 +2,20 @@
 
 Servo servo0;
 int val;
-
+int offset=98;
+int left_val = offset-10;
+int rigth_val = offset+10;
 void setup()
 {
   Serial.begin(9600);  
   servo0.attach(9);
+  servo0.write(offset);
 }
 
 void loop()
 {
     val = Serial.read();
+    //もし前の値と変わってたらの判定が必要（未対応）
     Serial.println(val);
     switch(val){
       //case -1:
@@ -30,17 +34,17 @@ void loop()
       break;
       
       case 113://q:left
-      servo0.write(110);
+      servo0.write(left_val);
       delay(100);
       break;
       
       case 101://e:right
-      servo0.write(70);
+      servo0.write(rigth_val);
       delay(100);
       break;
             
       case 119://w:
-      servo0.write(90);
+      servo0.write(offset);
       delay(100);
       break;
             
